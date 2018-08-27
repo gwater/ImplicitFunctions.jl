131411b765d63d9dc56c499f75836159e6eb4139
Josua Grawitter <josua.grawitter@campus.tu-berlin.de>
2018-08-27 13:58:56 +0200

    update for 1.0, fix eltype(), version 0.2.0

--- src/ImplicitFunctions.jl
+++ src/ImplicitFunctions.jl
@@ -4,19 +4,22 @@ using ForwardDiff
 using Tensors
 using NLsolve
 
-import Base: start, next, done, iteratorsize, iteratoreltype, eltype
+import Base: iterate, eltype, IteratorSize
 
 export ImplicitIterator
-export start, next, done, iteratorsize, iteratoreltype, eltype
+export iterate, eltype, IteratorSize
 
 jacobian(f, x::AbstractVector) =
     Tensors.gradient(x -> Vec{length(x)}(f(x)), Vec{length(x)}(x))
 jacobian(f, x::Real) = ForwardDiff.derivative(f, x)
 
 # https://math.stackexchange.com/questions/1415898/pseudo-arclength-continuation-scheme
-function arc_approx(f, x0, p0::P, dp::P) where {P <: Real}
+function arc_approx(f, x0, p0::P, dp::P, ::Type{Val{DEBUG}} = Val{false}) where {P <: Real, DEBUG}
     # tangent vector
-    dxdp = -jacobian(x -> f(x, p0), x0) \ jacobian(p -> f(x0, p), p0)
+    Jx = jacobian(x -> f(x, p0), x0)
+    DEBUG && info(J)
+    # iszero(det(Jx)) % throw some exception
+    dxdp = -Jx \ jacobian(p -> f(x0, p), p0)
     return x0 .+ dxdp * dp, p0 + dp
 end
 
@@ -24,12 +27,21 @@ struct ConvergenceException{T} <: Exception
     res::T
 end
 
-function _newton(f, x0, ::Type{Val{DEBUG}}) where DEBUG
+function _newton(f, x0, ::Type{Val{DEBUG}} = Val{false}) where DEBUG
     res = nlsolve(f, x0, method = :newton, inplace = false, autodiff = :forward)
     DEBUG && info(res)
     converged(res) && return res.zero
     throw(ConvergenceException(res))
 end
+
+function step(f, x0, p0, dp, ::Type{Val{DEBUG}} = Val{false}) where DEBUG
+    x1_approx, p1 = arc_approx(f, x0, p0, dp, Val{DEBUG})
+    x1 = _newton(x -> f(x, p1), x1_approx, Val{DEBUG})
+    return x1, p1
+end
+
+# Iterator interface
+# https://docs.julialang.org/en/v1.0.0/manual/interfaces/#man-interface-iteration-1
 
 struct ImplicitIterator{X, P <: Real, DEBUG, F}
     f::F
@@ -40,28 +52,19 @@ end
 ImplicitIterator(f::F, x0::X, p0::P, dp::P = 0.1; debug::Bool = false) where
     {X, P, F} = ImplicitIterator{X, P, debug, F}(f, x0, p0, dp)
 
-iteratorsize(::Type{I}) where {I <: ImplicitIterator} = Base.SizeUnknown()
-iteratoreltype(::Type{I}) where {I <: ImplicitIterator} = Base.HasEltype()
-eltype(iter::ImplicitIterator{X, P}) where {X, P} = Tuple{X, P}
+IteratorSize(::Type{I}) where {I <: ImplicitIterator} = Base.SizeUnknown()
 
-function start(iter::ImplicitIterator)
-    return iter.x0, iter.p0
+eltype(::Type{I}) where {X, P, I <: ImplicitIterator{X, P}} = Tuple{X, P}
+
+function iterate(iter::ImplicitIterator)
+    initial_state = (iter.x0, iter.p0)
+    return initial_state, initial_state
 end
 
-function next(iter::ImplicitIterator{X, P, DEBUG}, old_state) where {X, P, DEBUG}
+function iterate(iter::ImplicitIterator{X, P, DEBUG}, old_state) where {X, P, DEBUG}
     x0, p0 = old_state
-    x1_approx, p1 = arc_approx(iter.f, x0, p0, iter.dp)
-    x1 = _newton(x -> iter.f(x, p1), x1_approx, Val{DEBUG})
-    new_state = x1, p1
+    new_state = step(iter.f, x0, p0, iter.dp, Val{DEBUG})
     return new_state, new_state
-end
-
-function done(iter::ImplicitIterator{X, P, DEBUG}, state) where {X, P, DEBUG}
-    # stop if we cannot invert the Jacobian df/dx
-    x0, p0 = state
-    J = jacobian(x -> iter.f(x, p0), x0)
-    DEBUG && info(J)
-    return iszero(det(J)) # break at non-invertible Jacobian
 end
 
 end # module
